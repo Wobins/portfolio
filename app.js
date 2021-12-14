56fa175c761f7aad9ed181fdafd67c7cd025c0c8
Wobins <70920248+Wobins@users.noreply.github.com>
2021-12-14 16:12:08 +0100

    suppresion de fichiers obsoletes

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
 console.log("hello")
 
 
-let shortLinks = document.querySelectorAll('.shortLink');
-
-for (const link of shortLinks) {
-    link.addEventListener('mouseover', function() {
-        console.log('mouseover');
-        this.classList.toggle('linksHover');
-    })
-}
-
-
 //For the back-to-top button
 try {
 //Get the button
-let mybutton = document.getElementById("btn-back-to-top");
+let mybutton = document.querySelector("#btn-back-to-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
